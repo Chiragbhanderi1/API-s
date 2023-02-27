3a4c204bc4b5681a68f851373b0aae7bd8a6f7d3
Chirag <cbhanderi666@gmail.com>
2023-02-27 19:00:06 +0530

    commit

--- index.js
+++ index.js
@@ -2,19 +2,8 @@ const express = require('express');
 const app  = express();
 const Course = require('./modals/course');
 const admin = require('firebase-admin');
-require('dotenv').config();
-const credentials = {
-  type : process.env.TYPE,
-  project_id : process.env.PROJECT_ID,
-  private_key_id : process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
-  client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.AUTH_URI,
-  token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_x509_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_x509_CERT_URL
-};
+// require('dotenv').config();
+const credentials = require("./key.json");
 const multer = require('multer')
 
 admin.initializeApp({
