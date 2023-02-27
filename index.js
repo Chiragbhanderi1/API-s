e680dc65fabacdaa13197de754fc0d9d2f992885
Chirag <cbhanderi666@gmail.com>
2023-02-27 18:10:04 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app  = express();
 const Course = require('./modals/course');
 const admin = require('firebase-admin');
-const credentials = require('./key.json');
+const credentials = {
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_x509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_x509_CERT_URL
+};
 const multer = require('multer')
 
 admin.initializeApp({
