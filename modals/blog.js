fc8b70452edabba909dd04ba33fd6359ab51aba6
Chirag <cbhanderi666@gmail.com>
2023-04-05 14:49:01 +0530

    blogs

---
+++ modals/blog.js
@@ -0,0 +1,11 @@
+class Blog {
+    constructor( id, title, date,details,img ) {
+            this.id = id;
+            this.title = title;
+            this.date = date;
+            this.details = details;
+            this.img = img;
+    }
+}
+
+module.exports = Blog;
