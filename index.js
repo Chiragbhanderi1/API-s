833787a66b4cda557598f2e4198efa0a329274b4
Chirag <cbhanderi666@gmail.com>
2023-05-12 18:22:18 +0530

    cors

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const upload = multer({
     storage: multer.memoryStorage(),
   });
 const db =  admin.firestore();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // Admin Account system starts here
 app.get('/getadmins',async(req,res)=>{
